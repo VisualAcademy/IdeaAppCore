d902cd03f9467534fb12f9dddb00824663e0590a
Park YongJune <redplus@redplus.net>
2025-01-28 13:07:02 +0900

    Convert to file-scoped namespace

--- IdeaAppCore/Models/IdeaRepository.cs
+++ IdeaAppCore/Models/IdeaRepository.cs
@@ -3,27 +3,26 @@ using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace IdeaAppCore.Models
+namespace IdeaAppCore.Models;
+
+public class IdeaRepository : IIdeaRepository
 {
-    public class IdeaRepository : IIdeaRepository
+    private readonly ApplicationDbContext _context;
+
+    public IdeaRepository(ApplicationDbContext context)
     {
-        private readonly ApplicationDbContext _context;
+        this._context = context;
+    }
 
-        public IdeaRepository(ApplicationDbContext context)
-        {
-            this._context = context;
-        }
+    public async Task<Idea> AddIdea(Idea idea)
+    {
+        _context.Ideas.Add(idea);
+        await _context.SaveChangesAsync();
+        return idea; 
+    }
 
-        public async Task<Idea> AddIdea(Idea idea)
-        {
-            _context.Ideas.Add(idea);
-            await _context.SaveChangesAsync();
-            return idea; 
-        }
-
-        public async Task<List<Idea>> GetIdeas()
-        {
-            return await _context.Ideas.ToListAsync();
-        }
+    public async Task<List<Idea>> GetIdeas()
+    {
+        return await _context.Ideas.ToListAsync();
     }
 }
